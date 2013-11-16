d523698ee0242f845ccaecd26b08374177003d93
Bruce Steedman <bruce.steedman@gmail.com>
2013-11-16 20:29:34 +0000

    Refactored to build array in final loop, rather than string

--- numbers_to_roman_numerals/numbers_to_roman_numerals.rb
+++ numbers_to_roman_numerals/numbers_to_roman_numerals.rb
@@ -12,25 +12,24 @@ def number_to_roman_numeral(n)
   # need iterate through all elements unless array is 4 digits long
   limit = rev.size > 3 ? 3 : rev.size
   (0...limit).each do |i|
-
-    puts i
-    puts rev[i]
     numeral_count[i * 2 + 2], numeral_count[i * 2 + 1] =  rev[i] / 2, rev[i] % 2
-
-
-
   end
   # change 'M' to correctly relect 1,000's for big numbers
   numeral_count[6] = n / 1000 if numeral_count.size == 7
   # loop to build string, converting '1-less' numbers as appropriate
-  string = ""
-  p numeral_count.reverse
+  conversion = []
+  c = numeral_count.reverse
+  p c
   # reverse again to get in correct order to build string
-  numeral_count.reverse.each_with_index do |n,i|
+  c.each_with_index do |n,i|
     # first determine if the string is prepended with next numeral up
-    n == 4 ? string += "#{numerals[i]}#{numerals[i-1]}" : string += "#{numerals[i].to_s * n}"
+    if n == 4
+      c[i-1] = 0 ? conversion << "#{numerals[i]}#{numerals[i-1]}" : conversion << "#{numerals[i]}#{numerals[i-1]}"
+    else
+      conversion << "#{numerals[i].to_s * n}"
+    end
   end
-  string
+  string = conversion.join('')
 end
 
-puts number_to_roman_numeral(448)
+puts number_to_roman_numeral(9)

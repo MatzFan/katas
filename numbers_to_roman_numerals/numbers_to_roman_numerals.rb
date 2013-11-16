13db208e5e1558c8d1a081dd21a768a90112aaf3
Bruce Steedman <bruce.steedman@gmail.com>
2013-11-16 20:04:05 +0000

    ... well nearly - 4 rspec failures now, down from 9 - TBC

--- numbers_to_roman_numerals/numbers_to_roman_numerals.rb
+++ numbers_to_roman_numerals/numbers_to_roman_numerals.rb
@@ -8,15 +8,23 @@ def number_to_roman_numeral(n)
   # split number into array of digits
   by_5_arr = by_5.to_s.split('').map { |c| c.to_i }
   rev = by_5_arr.reverse
+  p rev
   # need iterate through all elements unless array is 4 digits long
   limit = rev.size > 3 ? 3 : rev.size
   (0...limit).each do |i|
-    numeral_count[i * 2 + 1], numeral_count[i * 2 + 2] = rev[i] % 2, rev[0] % 5
+
+    puts i
+    puts rev[i]
+    numeral_count[i * 2 + 2], numeral_count[i * 2 + 1] =  rev[i] / 2, rev[i] % 2
+
+
+
   end
   # change 'M' to correctly relect 1,000's for big numbers
   numeral_count[6] = n / 1000 if numeral_count.size == 7
   # loop to build string, converting '1-less' numbers as appropriate
   string = ""
+  p numeral_count.reverse
   # reverse again to get in correct order to build string
   numeral_count.reverse.each_with_index do |n,i|
     # first determine if the string is prepended with next numeral up

7173121b377ea0b27b52aebc6c3481a7f5babd47
Bruce Steedman <bruce.steedman@gmail.com>
2013-11-18 09:48:15 +0000

    Changed way of stripping "£","." and "p"

--- money_to_coins/money_to_coins.rb
+++ money_to_coins/money_to_coins.rb
@@ -1,5 +1,5 @@
-def money_to_coins(amount)
-  sum = (amount.chr == "£" ? amount.slice!(1..-1).to_f*100 : amount.slice(0..-1).to_i)
+def money_to_coins(sum)
+  sum = sum.gsub!(/[£.p]/, '').to_i
   coins = 1, 2, 5, 10, 20, 50, 100, 200
   syms = :"1p", :"2p", :"5p", :"10p", :"20p", :"50p", :"£1", :"£2"
   change = []
@@ -12,3 +12,5 @@ def money_to_coins(amount)
   change.reverse!.each_with_index { |n,i| string << "#{n}x#{syms[i]} " if n != 0 }
   string.strip
 end
+
+puts money_to_coins("£1.79")

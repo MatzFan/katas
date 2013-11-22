356ff6589559a834d52c8d075cc27f59a0861644
Bruce Steedman <bruce.steedman@gmail.com>
2013-11-22 15:17:11 +0000

    Added ability to deal with millions, but still bugs

--- numbers_to_words/number_to_words.rb
+++ numbers_to_words/number_to_words.rb
@@ -4,8 +4,6 @@ class Numbers
 UNIT_WORDS = %W[one two three four five six seven eight nine ten #{}]
 TEEN_WORDS = %W[eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen #{}]
 TENS_WORDS = %W[ten twenty thirty forty fifty sixty seventy eighty ninety #{}]
-# @magnitudes = %W[ten hundred thousand #{}]
-
 
   def number_to_words(n)
     words = ""
@@ -14,18 +12,24 @@ TENS_WORDS = %W[ten twenty thirty forty fifty sixty seventy eighty ninety #{}]
     return '' if n == 0
     units_map = Hash[(1..9).each.zip(UNIT_WORDS)]
     tens_map = Hash[(10..90).step(10).each.zip(TENS_WORDS)]
-    hundred_thousands, remainder = n.divmod(100_000)
-    ten_thousands = remainder.divmod(10000)
-    thousands, remainder = remainder.divmod(1000)
-    small_words_array = array(n % 1000)
 
-    big_words_array = (n >= 1000 ? ['thousand'] : []) + array(n/1000)
+    millions, thousands, rest = calc_000s(n)
+
+    very_big_words_array = (n >= 1_000_000 ? ['million'] : []) + array(n/1_000_000)
+    big_words_array = (n >= 1_000 ? ['thousand'] : []) + array(thousands)
+    small_words_array = array(rest)
 
     zeros = small_words_array.join('')
+    more_zeros = big_words_array.join('')
 
-    has_an_and = (n >= 1000) && (words_arr[2] == '0' && !zeros.empty?) ? ['and'] : [""]
+    has_an_and = (n >= 1_000) && (words_arr[2] == '0' && !zeros.empty?) ? ['and'] : [""]
+    has_another_and = (n >= 1_000_000) && (words_arr[5] == '0' && !more_zeros.empty?) ? ['and'] : [""]
 
-    words_arr = big_words_array.reverse!
+
+      words_arr = very_big_words_array.reverse!
+      words_arr << has_another_and
+
+    words_arr << big_words_array.reverse!
     words_arr << has_an_and
     words_arr += small_words_array.reverse!
 
@@ -39,6 +43,12 @@ TENS_WORDS = %W[ten twenty thirty forty fifty sixty seventy eighty ninety #{}]
     words = words_arr.join(' ').strip
     words.gsub!(/\s\s/, ' ')
     return words
+  end
+
+  def calc_000s(n)
+    millions, remainder = n.divmod(1_000_000)
+    thousands, remainder = remainder.divmod(1_000)
+    return [millions, thousands, remainder]
   end
 
   def array(n)
@@ -62,5 +72,5 @@ TENS_WORDS = %W[ten twenty thirty forty fifty sixty seventy eighty ninety #{}]
 
 end # of class
 
-# puts Numbers.new.number_to_words(145254)
+puts Numbers.new.number_to_words(1567843)
 

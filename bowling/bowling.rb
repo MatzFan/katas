6aaf4d6ba01df20248144cc695d0ada87c0820bb
Bruce Steedman <bruce.steedman@gmail.com>
2013-11-25 22:59:57 +0000

    Works for all numerical scores 1-9

--- bowling/bowling.rb
+++ bowling/bowling.rb
@@ -1,2 +1,11 @@
 def score_for(score_card)
+  score_arr = score_card.split('')
+  total_score = 0
+  score_arr.each do |score|
+    total_score += score.to_i if ('1'..'9').include? score
+  end
+  total_score
+
 end
+
+p score_for('22222222222222222222')

9788df3d13372dd9436ceda00e8ae199ee3ad730
Bruce Steedman <bruce.steedman@gmail.com>
2013-11-26 21:32:07 +0000

    Works for straight strikes

--- bowling/bowling.rb
+++ bowling/bowling.rb
@@ -1,11 +1,46 @@
+@total_score = 0
+
+SCORE_MAP = Hash[('1'..'9').to_a.zip(1..9)]
+SCORE_MAP['X'] = 10
+SCORE_MAP['/'] = 10
+
 def score_for(score_card)
   score_arr = score_card.split('')
-  total_score = 0
-  score_arr.each do |score|
-    total_score += score.to_i if ('1'..'9').include? score
+  score_arr << score_arr.last << score_arr.last
+  rev_score_arr = score_arr.reverse
+  p rev_score_arr
+  rev_score_arr.each_with_index do |symbol, index|
+    next if index < 2
+    @second_score = SCORE_MAP[rev_score_arr[index -1]]
+    @first_score = SCORE_MAP[rev_score_arr[index - 2]]
+    score = parse symbol
+    @total_score += score.to_i
   end
-  total_score
-
+  @total_score
 end
 
-p score_for('22222222222222222222')
+def parse(symbol)
+  case symbol
+  when ('1'..'9') then normal(symbol)
+  when 'X'
+    strike
+  when '/'
+    spare
+  end
+end
+
+def normal(sym)
+  sym.to_i
+end
+
+def strike
+  @total_score += @second_score
+  spare
+end
+
+def spare
+  @total_score += @first_score
+  10
+end
+
+p score_for('XXXXXXXXXX')
